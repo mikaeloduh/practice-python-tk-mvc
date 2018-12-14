b1123a0a6953926257224c1b2aee49552c9c1388
mikaeloduh <mikaeloduh@users.noreply.github.com>
2018-12-14 17:53:55 +0800

    Improves modal structures

--- cycletimegui/gear/gear.py
+++ cycletimegui/gear/gear.py
@@ -4,83 +4,40 @@ import os
 
 class Gear:
     def __init__(self):
-        self.data = {
-                        "workID": "untitled-project",
-                        "parts": {
-                            "productID": "no-name",
-                            "module": 0,
-                            "normPressAng_d": 0,
-                            "normPressAng_m": 0,
-                            "normPressAng_s": 0,
-                            "starts": 0,
-                            "pitchDia": 0,
-                            "outDia": 0,
-                            "rootDia": 0,
-                            "leadAng_d": 0,
-                            "leadAng_m": 0,
-                            "leadAng_s": 0,
-                            "toothThick": 0,
-                            "threadLen": 0
-                        },
-                        "machining": [
-                            {
-                                "passes": 0,
-                                "feedForw": 0,
-                                "feedBack": 0,
-                                "infeedForw": 0,
-                                "infeedBack": 0,
-                                "infeedTotal": 0,
-                                "plunge": 0,
-                                "speed": 0,
-                                "dressPass": 0,
-                                "dressInfeed": 0,
-                                "dressFeed": 0,
-                                "dressFrq": 0
-                            },
-                            {
-                                "passes": 0,
-                                "feedForw": 0,
-                                "feedBack": 0,
-                                "infeedForw": 0,
-                                "infeedBack": 0,
-                                "infeedTotal": 0,
-                                "plunge": 0,
-                                "speed": 0,
-                                "dressPass": 0,
-                                "dressInfeed": 0,
-                                "dressFeed": 0,
-                                "dressFrq": 0
-                            },
-                            {
-                                "passes": 0,
-                                "feedForw": 0,
-                                "feedBack": 0,
-                                "infeedForw": 0,
-                                "infeedBack": 0,
-                                "infeedTotal": 0,
-                                "plunge": 0,
-                                "speed": 0,
-                                "dressPass": 0,
-                                "dressInfeed": 0,
-                                "dressFeed": 0,
-                                "dressFrq": 0
-                            },
-                            {
-                                "passes": 0,
-                                "feedForw": 0,
-                                "feedBack": 0,
-                                "infeedForw": 0,
-                                "infeedBack": 0,
-                                "infeedTotal": 0,
-                                "plunge": 0,
-                                "speed": 0,
-                                "dressPass": 0,
-                                "dressInfeed": 0,
-                                "dressFeed": 0,
-                                "dressFrq": 0
-                            }
-                        ]
-                    }
+        self.__workspace = {}
+        self.__workspace['workID'] = 'untitled-project'
+
+        parts = {}
+        parts['productID'] = 'no-name'
+        parts['module'] = 0
+        parts['normPressAng_d'] = 0
+        parts['normPressAng_m'] = 0
+        parts['normPressAng_s'] = 0
+        parts['starts'] = 0
+        parts['pitchDia'] = 0
+        parts['outDia'] = 0
+        parts['rootDia'] = 0
+        parts['leadAng_d'] = 0
+        parts['leadAng_m'] = 0
+        parts['leadAng_s'] = 0
+        parts['toothThick'] = 0
+        parts['threadLen'] = 0
+        self.__workspace['parts'] = parts
+
+        machining = [{}]
+        machining[0]['passes'] = 0
+        machining[0]['feedForw'] = 0
+        machining[0]['feedBack'] = 0
+        machining[0]['infeedForw'] = 0
+        machining[0]['infeedBack'] = 0
+        machining[0]['infeedTotal'] = 0
+        machining[0]['plunge'] = 0
+        machining[0]['speed'] = 0
+        machining[0]['dressPass'] = 0
+        machining[0]['dressInfeed'] = 0
+        machining[0]['dressFeed'] = 0
+        machining[0]['dressFrq'] = 0        
+        self.__workspace['machining'] = machining * 4
 
         self.observers = []
         self.grindTime_1 = 0
@@ -100,42 +57,16 @@ class Gear:
 
     def notify(self):
         for observer in self.observers:
-            observer(self.data)
+            observer(self.__workspace)
 
     def setData(self, data):
-        self.data = data
-        #self.module = self.data["parts"]["module"]
-        self.starts = self.data["parts"]["starts"]
-        self.threadLen = self.data["parts"]["threadLen"]
-
-        self.passes_1 = self.data["machining"][0]["passes"]
-        self.feedForw_1 = self.data["machining"][0]["feedForw"]
-        self.dressPass_1 = self.data["machining"][0]["dressPass"]
-        self.dressFeed_1 = self.data["machining"][0]["dressFeed"]
-        self.byPass_1 = self.data["machining"][0]["dressFrq"]
-
-        self.passes_2 = self.data["machining"][1]["passes"]
-        self.feedForw_2 = self.data["machining"][1]["feedForw"]
-        self.dressPass_2 = self.data["machining"][1]["dressPass"]
-        self.dressFeed_2 = self.data["machining"][1]["dressFeed"]
-        self.byPass_2 = self.data["machining"][1]["dressFrq"]
-
-        self.passes_3 = self.data["machining"][2]["passes"]
-        self.feedForw_3 = self.data["machining"][2]["feedForw"]
-        self.dressPass_3 = self.data["machining"][2]["dressPass"]
-        self.dressFeed_3 = self.data["machining"][2]["dressFeed"]
-        self.byPass_3 = self.data["machining"][2]["dressFrq"]
-
-        self.passes_4 = self.data["machining"][3]["passes"]
-        self.feedForw_4 = self.data["machining"][3]["feedForw"]
-        self.dressPass_4 = self.data["machining"][3]["dressPass"]
-        self.dressFeed_4 = self.data["machining"][3]["dressFeed"]
-        self.byPass_4 = self.data["machining"][3]["dressFrq"]
+        self.__workspace = data
+        
         # notify
         self.notify()
 
     def getData(self):
-        return self.data
+        return self.__workspace
 
     def toCalculate(self):
         self.grindTime_1 = 0
@@ -148,24 +79,51 @@ class Gear:
         self.dressTime_4 = 0
         self.cycleTime = 0
 
-        if self.passes_1:
-            self.grindTime_1 = 6.0 + self.starts * self.passes_1 * self.threadLen / self.feedForw_1 * (41*200/1.68)
-        if self.byPass_1:
-            self.dressTime_1  = ( 12 + 19*6 * self.dressPass_1 / self.dressFeed_1 + 12 ) * (self.starts * self.passes_1 // self.byPass_1)
+        starts = self.__workspace["parts"]["starts"]
+        threadLen = self.__workspace["parts"]["threadLen"]
 
-        if self.passes_2:
-            self.grindTime_2 = 6.0 + self.starts * self.passes_2 * self.threadLen / self.feedForw_2 * (41*200/1.68)
-        if self.byPass_2:
-            self.dressTime_2  = ( 12 + 19*6 * self.dressPass_2 / self.dressFeed_2 + 12 ) * (self.starts * self.passes_2 // self.byPass_2)
+        passes_1 = self.__workspace["machining"][0]["passes"]
+        feedForw_1 = self.__workspace["machining"][0]["feedForw"]
+        dressPass_1 = self.__workspace["machining"][0]["dressPass"]
+        dressFeed_1 = self.__workspace["machining"][0]["dressFeed"]
+        byPass_1 = self.__workspace["machining"][0]["dressFrq"]
 
-        if self.passes_3:
-            self.grindTime_3 = 6.0 + self.starts * self.passes_3 * self.threadLen / self.feedForw_3 * (41*200/1.68)
-        if self.byPass_4:
-            self.dressTime_3  = ( 12 + 19*6 * self.dressPass_3 / self.dressFeed_3 + 12 ) * (self.starts * self.passes_3 // self.byPass_3)
+        passes_2 = self.__workspace["machining"][1]["passes"]
+        feedForw_2 = self.__workspace["machining"][1]["feedForw"]
+        dressPass_2 = self.__workspace["machining"][1]["dressPass"]
+        dressFeed_2 = self.__workspace["machining"][1]["dressFeed"]
+        byPass_2 = self.__workspace["machining"][1]["dressFrq"]
 
-        if self.passes_4:
-            self.grindTime_4 = 6.0 + self.starts * self.passes_4 * self.threadLen / self.feedForw_4 * (41*200/1.68)
-        if self.byPass_4:
-            self.dressTime_4  = ( 12 + 19*6 * self.dressPass_4 / self.dressFeed_4 + 12 ) * (self.starts * self.passes_4 // self.byPass_4)
+        passes_3 = self.__workspace["machining"][2]["passes"]
+        feedForw_3 = self.__workspace["machining"][2]["feedForw"]
+        dressPass_3 = self.__workspace["machining"][2]["dressPass"]
+        dressFeed_3 = self.__workspace["machining"][2]["dressFeed"]
+        byPass_3 = self.__workspace["machining"][2]["dressFrq"]
+
+        passes_4 = self.__workspace["machining"][3]["passes"]
+        feedForw_4 = self.__workspace["machining"][3]["feedForw"]
+        dressPass_4 = self.__workspace["machining"][3]["dressPass"]
+        dressFeed_4 = self.__workspace["machining"][3]["dressFeed"]
+        byPass_4 = self.__workspace["machining"][3]["dressFrq"]
+
+        if passes_1:
+            self.grindTime_1 = 6.0 + starts * passes_1 * threadLen / feedForw_1 * (41*200/1.68)
+        if byPass_1:
+            self.dressTime_1  = ( 12 + 19*6 * dressPass_1 / dressFeed_1 + 12 ) * (starts * passes_1 // byPass_1)
+
+        if passes_2:
+            self.grindTime_2 = 6.0 + starts * passes_2 * threadLen / feedForw_2 * (41*200/1.68)
+        if byPass_2:
+            self.dressTime_2  = ( 12 + 19*6 * dressPass_2 / dressFeed_2 + 12 ) * (starts * passes_2 // byPass_2)
+
+        if passes_3:
+            self.grindTime_3 = 6.0 + starts * passes_3 * threadLen / feedForw_3 * (41*200/1.68)
+        if byPass_4:
+            self.dressTime_3  = ( 12 + 19*6 * dressPass_3 / dressFeed_3 + 12 ) * (starts * passes_3 // byPass_3)
+
+        if passes_4:
+            self.grindTime_4 = 6.0 + starts * passes_4 * threadLen / feedForw_4 * (41*200/1.68)
+        if byPass_4:
+            self.dressTime_4  = ( 12 + 19*6 * dressPass_4 / dressFeed_4 + 12 ) * (starts * passes_4 // byPass_4)
 
         self.cycleTime = self.dressTime_1 + self.grindTime_1 + self.dressTime_2 + self.grindTime_2 + self.dressTime_3 + self.grindTime_3 + self.dressTime_4 + self.grindTime_4
